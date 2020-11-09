831f05b1827eb9d8a36cc94b8a3bdd22a22ef034
viniciusmfelix <vinicius.mfelix@outlook.com>
2020-11-09 13:06:40 -0300

    First commit

---
+++ src/main/java/br/com/fiap/babysitter/BabysitterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.babysitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabysitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabysitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/babysitter/BabysitterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.babysitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabysitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
